b66df4000a5a66d78b5ff75dc77a1244643b47ab
Evgenij Grines <evgenij.grines@itseez.com>
2017-10-18 14:57:59 +0400

    Extracted Configuration class from main file

---
+++ configuration.h
@@ -0,0 +1,44 @@
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+
+#include <iostream>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include "ash5.h"
+#include <map>
+
+struct Configuration
+{
+public:
+    double rInit, aInit, bInit;
+    double rtol, atol, eventTol, approximateReturnTime, fixPtTol;
+    int fixPtMaxIter;
+    AshStateType initialFixedPoint;
+    std::string v1_name, v2_name;
+    double v1_min, v1_max, v2_min, v2_max;
+    int v2_N, v1_N;
+    int mapIterSkip, mapIterLast;
+    struct UsageFlags
+    {
+        bool isEigvalsNeeded;
+        bool isLyapNeeded;
+        bool isDistNeeded;
+        bool isPlotNeeded;
+        UsageFlags(): isEigvalsNeeded(false), isLyapNeeded(false), isDistNeeded(false), isPlotNeeded(false)
+        {
+            ;
+        }
+    };
+    UsageFlags flags;
+    double crossingDirection;
+    bool isConfigurationConsistent;
+    double jacobiNewtonEps, jacobiFixPtTypeEps;
+    double skipTime;
+    /////////////////////////
+    Configuration(int argc, char* argv[]);
+    std::map<std::string, double> getParameterValues(int i, int j) const;
+private:
+    bool checkConfigFileForConsistency(boost::property_tree::ptree config) const;
+};
+
+#endif // CONFIGURATION_H
