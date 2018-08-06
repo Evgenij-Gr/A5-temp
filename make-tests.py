c4d3971583221dc105b1d7ed6444f2a98429fc66
Evgenij Grines <evgenij.grines@itseez.com>
2018-08-06 13:16:52 +0400

    Removed unnecessary testing script Removed qmake project file Added CMakeLists.txt for CMake project generation

--- make-tests.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import subprocess
-
-subprocess.call('g++ -o ash5tests-exe --std=c++11 ash5-tests.cpp ash5.cpp configuration.cpp', shell=True)
-subprocess.call('./ash5tests-exe')
